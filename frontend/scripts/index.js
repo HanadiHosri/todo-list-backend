3ee9f527a9656c8c889209d063c238ce26209656
unknown <hanadi.hosri@gmail.com>
2024-03-15 12:12:55 +0200

    finalizing signUp function

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -2,13 +2,16 @@ function login() {
     var username = document.getElementById("username");
     var password = document.getElementById("password");
     if(username.value == "AdminSEF123" && password.value == "SeF@ctORy$$456") {
-        window.location.href = "../main.html"
+        window.location.href = "main.html"
     } else {
         alert("Invalid username or password. Please try again.")
     }
 }
 
-
+const loginBtn = document.getElementById("login-btn");
+const signupBtn = document.getElementById("signup-btn");
+const loginText = document.getElementById("login-text");
+const signupText = document.getElementById("signup-text");
 
 
 const signUp = async () => {
@@ -28,11 +31,16 @@ const signUp = async () => {
             mode: 'no-cors'
         });
 
-        console.log(response);
-
         const responseData = await response.text();
         console.log("response data:", responseData);
-        window.location.href = "main.html"
+        loginBtn.classList.remove("hidden");
+        loginText.classList.remove("hidden");
+        signupBtn.classList.add("hidden");
+        signupText.classList.add("hidden");
+        document.getElementById("email").value="";
+        document.getElementById("username").value="";
+        document.getElementById("password").value="";
+
     } catch (error) {
         console.error("error:", error);
         alert("Error occurred while sending request");
